a4f032265474bd6bb6341d7487fadf0ed141010f
Choi Jongmin <choipeanut09@gmail.com>
2025-01-30 18:02:00 +0900

    fuckyou

--- JongMin/QtOpenGL_Jongmin/main.cpp
+++ JongMin/QtOpenGL_Jongmin/main.cpp
@@ -1,11 +1,22 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QSurfaceFormat>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    QCoreApplication::setAttribute(Qt::AA_UseDesktopOpenGL); // OpenGL 강제 적용
+    QSurfaceFormat format;
+    format.setDepthBufferSize(24);
+    format.setStencilBufferSize(8);
+    format.setVersion(4, 0);  // OpenGL 4.5 이상 사용
+    format.setProfile(QSurfaceFormat::CoreProfile);
+    QSurfaceFormat::setDefaultFormat(format);
+
+    QApplication app(argc, argv);
+
     MainWindow w;
     w.show();
-    return a.exec();
+
+    return app.exec();
 }
